11a94ac1f3c00a0a39fff71b998776738baed0bc
Joseph Izaguirre <izaguirrejoe@gmail.com>
2022-11-07 17:30:57 -0800

    Working on it

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -22,4 +22,3 @@ setBasePath("/shoelace-assets")
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
